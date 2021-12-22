1cd0d037c4ad994a2de37c12415a00a077fa1fe3
Nicolas Brassard <awesomecuber10401@gmail.com>
2021-12-21 18:11:35 -0800

    finished day 21

--- 2021/day21/day21_b.py
+++ 2021/day21/day21_b.py
@@ -3,7 +3,7 @@ import pprint
 import os
 import sys
 
-with open(os.path.join(sys.path[0], "day21ex.txt")) as f:
+with open(os.path.join(sys.path[0], "day21.txt")) as f:
     puzzle_input = f.read().splitlines()
 
 player1_pos = int(puzzle_input[0][-1])
@@ -16,27 +16,57 @@ num_dice_rolled = 0
 
 dice_roll = 0
 
+map = {
+    3: 1,
+    4: 3,
+    5: 6,
+    6: 7,
+    7: 6,
+    8: 3,
+    9: 1
+}
+
 @cache
-def num_wins(p1_pos, p1_score, p2_pos, p2_score, turn):
+def num_win_universes(p1_pos, p1_score, p2_pos, p2_score, turn):
     if p1_score >= 21:
         return 1, 0
     if p2_score >= 21:
         return 0, 1
     if turn == 1:
-        new_p1_pos = (p1_pos % 10) + 1
-        p1wins_1, p2wins_1 = num_wins(new_p1_pos, p1_score + new_p1_pos, p2_pos, p2_score, 2)
-        new_p1_pos = (p1_pos % 10) + 1
-        p1wins_2, p2wins_2 = num_wins(new_p1_pos, p1_score + new_p1_pos, p2_pos, p2_score, 2)
-        new_p1_pos = (p1_pos % 10) + 1
-        p1wins_3, p2wins_3 = num_wins(new_p1_pos, p1_score + new_p1_pos, p2_pos, p2_score, 2)
-        return p1wins_1 + p1wins_2 + p1wins_3, p2wins_1 + p2wins_2 + p2wins_3
+        p1_win_universes, p2_win_universes = 0, 0
+        for increment, times in map.items():
+            new_p1_pos = (p1_pos + increment - 1) % 10 + 1
+            new_p1_win_universes, new_p2_win_universes = num_win_universes(
+                new_p1_pos,
+                p1_score + new_p1_pos,
+                p2_pos,
+                p2_score,
+                2
+            )
+            p1_win_universes += times * new_p1_win_universes
+            p2_win_universes += times * new_p2_win_universes
+        return p1_win_universes, p2_win_universes
     if turn == 2:
-        new_p2_pos = (p1_pos % 10) + 1
-        p1wins_1, p2wins_1 = num_wins(p1_pos, p1_score, new_p2_pos, p2_score + new_p2_pos, 1)
-        new_p2_pos = (p1_pos % 10) + 1
-        p1wins_2, p2wins_2 = num_wins(p1_pos, p1_score, new_p2_pos, p2_score + new_p2_pos, 1)
-        new_p2_pos = (p1_pos % 10) + 1
-        p1wins_3, p2wins_3 = num_wins(p1_pos, p1_score, new_p2_pos, p2_score + new_p2_pos, 1)
-        return p1wins_1 + p1wins_2 + p1wins_3, p2wins_1 + p2wins_2 + p2wins_3
+        p1_win_universes, p2_win_universes = 0, 0
+        for increment, times in map.items():
+            new_p2_pos = (p2_pos + increment - 1) % 10 + 1
+            new_p1_win_universes, new_p2_win_universes = num_win_universes(
+                p1_pos,
+                p1_score,
+                new_p2_pos,
+                p2_score + new_p2_pos,
+                1
+            )
+            p1_win_universes += times * new_p1_win_universes
+            p2_win_universes += times * new_p2_win_universes
+        return p1_win_universes, p2_win_universes
 
-print(num_wins(player1_pos, player1_score, player2_pos, player2_score, 1))
+p1_universes, p2_universes = num_win_universes(
+    player1_pos,
+    player1_score,
+    player2_pos,
+    player2_score,
+    1
+)
+
+print(max(p1_universes, p2_universes))
